5c674e7adec6e371314f453bc75a17de5181a243
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-25 15:31:56 +0530

    Added to CreateStack

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+namespace StacksAndQueues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Stacks and Queues program");
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Create Stack of 56->30->70");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Stack s1 = new Stack();
+                        s1.Push(70);
+                        s1.Push(30);
+                        s1.Push(56);
+                        s1.display();
+                        break;
+                    default:
+                        Console.WriteLine("Please choose correct option from the list");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
---
+++ StacksAndQueues/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Stack
+    {
+        internal Node top;
+
+        //For initializing top to null
+        public Stack()
+        {
+            top = null;
+        }
+
+        //For pushing/(adding node at first position) the node onto the stack
+        internal void Push(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine(newNode.data + " is pushed/inserted onto the stack");
+        }
+
+        //For displaying theh stack
+        internal void display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("The Stack is :\n");
+            Console.Write(" top ->");
+            Node tempNode = top;
+
+            while (tempNode != null)
+            {
+                Console.WriteLine("\t| " + tempNode.data + " |");
+                tempNode = tempNode.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
