d6d04bde0a1dd7b19185b3a2aa05a99a38b64e12
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-25 15:41:25 +0530

    Added to CreateQueue

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -11,6 +11,7 @@ namespace StacksAndQueues
             {
                 Console.WriteLine("\n1. Create Stack of 56->30->70");
                 Console.WriteLine("2. Peek and Pop till the stack is empty");
+                Console.WriteLine("3. Create Queue of 56->30->70");
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -47,6 +48,13 @@ namespace StacksAndQueues
                         s2.Pop();
                         Console.Write("After pop operation ");
                         s2.display();
+                        break;
+                    case 3:
+                        Queue q1 = new Queue();
+                        q1.Enqueue(56);
+                        q1.Enqueue(30);
+                        q1.Enqueue(70);
+                        q1.Display();
                         break;
                     default:
                         Console.WriteLine("Please choose correct option from the list");
---
+++ StacksAndQueues/Queue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Queue
+    {
+        internal Node head = null;
+
+        //For inserting the node at the end of the queue
+        internal void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+
+            //If queue is empty insert nowNode at first position
+            if (head == null)
+            {
+                head = newNode;
+            }
+            //else insert at the end of our queue/linkedlist
+            else
+            {
+                Node tempHead = head;
+                while (tempHead.next != null)
+                {
+                    tempHead = tempHead.next;
+                }
+                tempHead.next = newNode;
+            }
+            Console.WriteLine(newNode.data + " is enqueued/inserted into the queue");
+        }
+
+        //For displaying nodes present in the queue
+        internal void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            else
+            {
+                Console.Write("Queue is : front/head");
+                Node tempHead = head;
+                while (tempHead != null)
+                {
+                    Console.Write(" -> " + tempHead.data);
+                    tempHead = tempHead.next;
+                }
+                Console.WriteLine(" <- rear/tail\n");
+            }
+        }
+    }
+}
